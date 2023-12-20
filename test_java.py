ebd9fd8f5c78a30f8c831cc00109a17271606a7e
kudos5566 <45815056+kudos5566@users.noreply.github.com>
2023-12-20 15:50:04 +0800

    Update test_java.py

--- test_java.py
+++ test_java.py
@@ -1,7 +1,9 @@
 import os
 import concurrent.futures
 import logging
+import re
 import sys
+
 
 class JavaScanner:
 
@@ -15,44 +17,52 @@ class JavaScanner:
         return code
 
     def scan_java_file(self, code):
-        results = []
+        found_keywords = {}
+        current_method = None
         line_number = 1
         for line in code.splitlines():
+            method_match = re.search(r"(public|protected|private|static|\s) +[\w\<\>\[\]]+\s+(\w+) *\([^\)]*\) *(\{"
+                                     r"?|[^;])", line)
+            if method_match:
+                current_method = method_match.group(2)
             for keyword in self.keywords:
-                if keyword in line:
-                    results.append({
-                        "keyword": keyword,
-                        "line_number": line_number,
-                    })
+                keyword_regex = r"%s" % keyword
+                if re.search(keyword_regex, line) and current_method:
+                    if keyword not in found_keywords:
+                        found_keywords[keyword] = {}
+                    if current_method not in found_keywords[keyword]:
+                        found_keywords[keyword][current_method] = []
+                    found_keywords[keyword][current_method].append(line_number)
             line_number += 1
-        return results
+        return found_keywords
 
     def scan_java_files(self):
         executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
-        results = []
+        result = []
         for root, dirs, files in os.walk(self.root_path):
             for file in files:
                 file_path = os.path.join(root, file)
                 if os.path.splitext(file)[1] in ALLOWED_EXTENSIONS:
                     try:
                         task = executor.submit(self.read_java_file, file_path)
-                        results.append(task)
+                        result.append((file_path, task))
                     except FileNotFoundError:
                         logging.warning(f"File not found: {file_path}")
 
-        for task in results:
+        for file_path, task in result:
             try:
                 code = task.result()
                 results = self.scan_java_file(code)
-                for result in results:
-                    print(f"{file_path}:{result['line_number']}: {result['keyword']}")
+                for keyword, methods in results.items():
+                    for method, line_numbers in methods.items():
+                        print(f"{file_path}-> {method}：{','.join(map(str, line_numbers))} = {keyword}")
             except Exception as e:
                 logging.error(e)
 
 
 if __name__ == "__main__":
     ALLOWED_EXTENSIONS = [".java", ".html", ".css", ".js", ".xml", "jsp"]
-    root_path = sys.argv[1]
+    root_path = sys.args[1]
     keywords = [
         "security", "StpUtil", "shiro", "OAuth", "Permission"
     ]
